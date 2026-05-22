20edbde83218d60d0b7dd1f0fb24b8b3314a27de
ddnemet <plenlover11@gmail.com>
2026-05-22 16:04:09 +0200

    Set APP_VERSION to 1.8.1
    Update APP_VERSION to match the latest release.

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ function playNotifSound() {
 }
 
 const GITHUB_REPO = "plen-maker/oiltrade";
-const APP_VERSION = "__APP_VERSION__"; // ezt növeld minden release-nél — egyezzen a GitHub tag-gel!
+const APP_VERSION = "1.8.1"; // ezt növeld minden release-nél — egyezzen a GitHub tag-gel!
 
 function compareVersions(a, b) {
   // true ha b újabb mint a
